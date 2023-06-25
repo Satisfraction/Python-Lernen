e16edbc24fb6c979791da5e7bedf0f1b99bf2553
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-06-25 20:13:23 +0200

    Update todo_list.py

--- Schritt 3 Projektstruktur und User Input/todo_list.py
+++ Schritt 3 Projektstruktur und User Input/todo_list.py
@@ -1,19 +1,18 @@
 # Fülle die Lücken entsprechend der Anweisung in der README.md
 
-import ___
+import ___      # hier importieren wir die datetime Funktion
 
-todo_list = []
+todo_list = []      # hier definieren wir die Liste
 
 def ___():
-    task = input("Gib eine Aufgabe ein: ")
-    ___
-
-if __name__ == "___":
+    task = input("Gib eine Aufgabe ein: ")      # hier wird die Aufgabe eingetragen mittels der input Funktion
+    ___                                # hier wird die Aufgabe ausgegeben mittels return Funktion
+if __name__ == "___":               
     while True:
-        ___ = add_task()
-        todo_list.append((___, ___.datetime.now()))
-        ___(f"Aufgabe '{task}' wurde zur To-Do-Liste hinzugefügt.")
+        ___ = add_task()                    # hier wird rufen wir die add_task Funktion auf
+        todo_list.append((___, ___.datetime.now()))      # hier wird die Aufgabe in der Liste hinzugefügt mittels der append Funktion
+        ___(f"Aufgabe '{task}' wurde zur To-Do-Liste hinzugefügt.")      # hier wird die Aufgabe ausgegeben mittels der print Funktion
 
-        action = input("Möchtest du eine weitere Aufgabe hinzufügen? (Ja/Nein): ")
-        if action.lower() != "ja":
+        action = input("Möchtest du eine weitere Aufgabe hinzufügen? (Ja/Nein): ")      # hier wird die Abfrage vom User geholt um Fortzusetzen mittels der input Funktion
+        if action.lower() != "ja":                # hier wird Programm abgebrochen, wenn die Abfrage nicht "Ja" ist
             ___
