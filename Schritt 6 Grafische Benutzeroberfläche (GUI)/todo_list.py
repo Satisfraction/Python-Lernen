42ff4a6f9ac0cd4c2b5dc48a43df9810ca793eab
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-07-02 22:35:08 +0200

    Create todo_list.py

---
+++ Schritt 6 Grafische Benutzeroberfläche (GUI)/todo_list.py
@@ -0,0 +1,56 @@
+# Folge den Anweisungen in der README.md von Schritt 6
+
+# Importe hier einfügen
+
+
+# Laden der To-Do-Liste
+# Funktion zum Laden der To-Do-Liste hier einfügen
+
+
+# Speichern der To-Do-Liste
+# Funktion zum Speichern der To-Do-Liste hier einfügen
+
+
+# Markieren einer Aufgabe als erledigt
+# Funktion zum Markieren einer Aufgabe als erledigt hier einfügen
+
+
+# Hinzufügen einer Aufgabe
+# Funktion zum Hinzufügen einer Aufgabe hier einfügen
+
+
+# Öffnen der Benutzeroberfläche
+# Funktion zum Öffnen der Benutzeroberfläche hier einfügen
+
+
+    # Funktion zum Laden der To-Do-Liste hier einfügen
+    # Laden der To-Do-Liste
+    
+
+    # Funktion zum Erstellen der Listbox hier einfügen
+    # Erstellen der Listbox
+    
+
+    # Funktion für das Hinzufügen der Vorhandenen Aufgaben hier einfügen
+    # Hinzufügen der vorhandenen Aufgaben zur Listbox
+    
+
+    # Funktion zum Erstellen des Eingabefelds hier einfügen
+    # Erstellen des Eingabefelds und der Schaltfläche zum Hinzufügen von Aufgaben
+    
+
+    # Funktion zum Erstellen der Schaltfläche zum Markieren einer Aufgabe hier einfügen
+    # Erstellen der Schaltfläche zum Markieren einer Aufgabe als erledigt
+    
+
+    # Funktion zum Schließen der GUI und Speichern der Liste hier einfügen
+    # Schließen der GUI und Speichern der Liste beim Beenden des Programms
+    
+
+    # Befehl zum Starten der Hauptschleife hier einfügen
+    # Start der Hauptschleife der Benutzeroberfläche
+    
+
+# Codeblock zum Ausführen der Anwendung hier einfügen
+# Öffnen der Benutzeroberfläche, wenn das Skript direkt ausgeführt wird
+
